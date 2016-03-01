ebc80398dfd2735a436adbb1070b91c09a164963
tonyli7 <xxtonyxxlixx@gmail.com>
2016-03-01 10:59:44 -0500

    mult_matrix complete

--- main.py
+++ main.py
@@ -3,17 +3,23 @@ from draw import *
 
 screen = new_screen()
 color = [ 0, 255, 0 ]
-
+"""
 mat=new_matrix()
 print_matrix( mat)
 ident(mat)
 print_matrix( mat)
 scalar_mult(mat, 3)
 print_matrix( mat)
+"""
 m1=new_matrix(3,2)
 m2=new_matrix(2,3)
-m1=[[1,2,3], [4,5,6]]
-m2=[[10,40],[20,50],[30,60]]
+m1=[[1,2,3], [4,5,6], [7,8,9]]
+m2=[[10,40,20],[20,50,30],[30,60,40]]
+print "m1"
+print_matrix(m1)
+print "m2"
+print_matrix(m2)
+print "m1*m2"
 print_matrix( matrix_mult(m1,m2))
 
-display(screen)
+#display(screen)
--- matrix.py
+++ matrix.py
@@ -51,18 +51,20 @@ def scalar_mult( matrix, x ):
 
 #m1 * m2 -> m2
 def matrix_mult( m1, m2 ):
-    if not len(m1[0]) == len(m2):
-        print "length of matrix-1 does not match height of matrix-2"
+    if not len(m1) == len(m2[0]):
+        print "Error: length of matrix-1 does not match height of matrix-2\n"
         return -1
     new=new_matrix(len(m1),len(m2[0]))
-    ctr1=0
-    t=0
+    ctr=0
+   
     for i in range(len(m1)):
+        t=0
         while t < len(m1):
-            for j in range(len(m2[0])):
-                new[ctr1][t]+=m1[i][j]*m2[j][i]
+            for j in range(len(m2)):
+                new[ctr][t]+=m1[i][j]*m2[j][t]
+                #print str(m1[i][j])+" * "+str(m2[j][t]) , ctr, t
             t+=1
-        ctr1+=1
+        ctr+=1
     return new
     pass
 
